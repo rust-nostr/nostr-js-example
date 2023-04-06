b800d6ba175155399e57c0bc9c68a7b3f46d2d09
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-04-06 16:09:39 +0200

    Bump `nostr` to v0.3.1
    Closes #1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import { Keys } from '@rust-nostr/nostr';
+import { Keys, loadWasmAsync } from '@rust-nostr/nostr';
+import { Component } from 'react';
 
-function App() {
-  let keys = Keys.generate();
-  let hex = keys.publicKey.toHex();
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { hex: null };
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {hex}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+    await loadWasmAsync();
+
+    let keys = Keys.generate();
+    let hex = keys.publicKey.toHex();
+    this.setState({ hex });
+  }
+
+  render() {
+    return (
+       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.hex}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
